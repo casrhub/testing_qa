ca283e70a24e0aa3ed14dd242b9c18cebfa08b76
César Silva <casrr@proton.me>
2025-03-24 14:32:57 -0600

    added tests to be

---
+++ backend/springboot-backend/src/test/test10.spec.js
@@ -0,0 +1,41 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('assert');
+const fs = require('fs');
+
+describe('Test 10', function () {
+    this.timeout(30000);
+    let driver;
+
+    if (!fs.existsSync('./screenshots')) {
+        fs.mkdirSync('./screenshots');
+    }
+
+    beforeEach(async function () {
+        const chrome = require('selenium-webdriver/chrome');
+        const options = new chrome.Options();
+        options.addArguments('--headless', '--no-sandbox', '--disable-dev-shm-usage');
+        driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+    });
+
+    afterEach(async function () {
+        try {
+            const alert = await driver.switchTo().alert();
+            console.warn("Dismissing alert:", await alert.getText());
+            await alert.accept();
+        } catch (e) {}
+        const filename = this.currentTest.fullTitle().replace(/['"]+/g, '').replace(/[^a-z0-9]/gi, '_').toLowerCase();
+        const screenshot = await driver.takeScreenshot();
+        await fs.promises.writeFile(`./screenshots/${filename}.png`, screenshot, 'base64');
+        if (driver) await driver.quit();
+    });
+
+ 
+it('10 - 4, should display: 6', async function () {
+    await driver.get("http://127.0.0.1:8000/index.html");
+    await driver.findElement(By.id("num1")).sendKeys("10");
+    await driver.findElement(By.id("num2")).sendKeys("4");
+    await driver.findElement(By.css("button:nth-child(2)")).click();
+    
+});
+
+});
