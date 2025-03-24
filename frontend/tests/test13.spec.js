05f4388042e11928c8a244b40845c57d383e1458
César Silva <casrr@proton.me>
2025-03-24 14:48:23 -0600

    added tests to be

--- frontend/tests/test13.spec.js
+++ frontend/tests/test13.spec.js
@@ -3,7 +3,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
 const fs = require('fs');
 
-describe('Test 13', function () {
+describe('-1 + -1 Test', function () {
     this.timeout(30000);
     let driver;
     let vars;
@@ -27,17 +27,28 @@ describe('Test 13', function () {
                 .replace(/[^a-z0-9]/gi, '_')
                 .toLowerCase();
             const encodedString = await driver.takeScreenshot();
-            await fs.promises.writeFile(`./screenshots/${filename}.png`, encodedString, 'base64');
+            await fs.writeFileSync(`./screenshots/${filename}.png`, encodedString, 'base64');
             await driver.quit();
         }
     });
 
-  it('Test 13 ', async function() {
-    await driver.get("http://127.0.0.1:8000/index.html")
-    await driver.findElement(By.id("num1")).click()
-    await driver.findElement(By.id("num1")).sendKeys("1000000")
-    await driver.findElement(By.id("num2")).click()
-    await driver.findElement(By.id("num2")).sendKeys("5000000")
-    await driver.findElement(By.css("button:nth-child(1)")).click()
-  })
-})
+    it('-1 + -1 should return -2', async function () {
+        await driver.get("http://127.0.0.1:8000/index.html");
+        await driver.manage().window().setRect({ width: 720, height: 900 });
+
+        await driver.findElement(By.id("num1")).click();
+        await driver.findElement(By.id("num1")).sendKeys("-1");
+
+        await driver.findElement(By.id("num2")).click();
+        await driver.findElement(By.id("num2")).sendKeys("-1");
+
+        await driver.findElement(By.xpath("//button[text()='+']")).click();
+
+        let resultText = await driver.findElement(By.id("result")).getText();
+        assert.strictEqual(resultText, "Result: -2");
+
+        const filename = 'test1';
+        const encodedString = await driver.takeScreenshot();
+        await fs.writeFileSync(`./screenshots/${filename}.png`, encodedString, 'base64');
+    });
+});
